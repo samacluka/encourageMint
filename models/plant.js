d5d1ab1d0e81fd3b6628dc93dbe7985240063b89
Luka Samac <1lukasamac@gmail.com>
2020-02-07 10:17:20 -0500

    Added logs to plant schema

--- models/plant.js
+++ models/plant.js
@@ -1,11 +1,13 @@
 var mongoose = require("mongoose");
 
 var User = require('./user.js');
+var Log = require('./log.js');
 
 var plantSchema = new mongoose.Schema({
   Name: String,
   Type: String,
   Owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  Logs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Log' }],
   soilMoisture: {
     max: Number,
     min: Number
