8bf2003247d3927eeac0250f6377938e6b1a2e41
Luka Samac <1lukasamac@gmail.com>
2020-02-29 14:19:29 -0500

    Intialized a default plant schema for the default params to plant types

---
+++ models/default.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var User = require('./plant.js');
+
+var defaultSchema = new mongoose.Schema({
+  Type: String,
+  soilMoisture: {
+    max: Number,
+    min: Number
+  },
+  lightThreshold: {
+    max: Number,
+    min: Number
+  }
+}, { collection: 'Default' });
+
+module.exports = mongoose.model("Default", defaultSchema);
